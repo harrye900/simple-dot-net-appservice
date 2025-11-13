5b646812e2015f44cf4c6606ebafc5bf1aea46d1
harry <harifosa@gmail.com>
2025-11-13 14:18:44 -0500

    Trigger deployment to Azure Web App

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "I Wll be great, Hello World! Simple .NET Web App deployed to Azure!");
+app.MapGet("/", () => "Hello World! Simple .NET Web App deployed to Azure!");
 
 app.MapGet("/health", () => new { Status = "Healthy", Timestamp = DateTime.UtcNow });
 
